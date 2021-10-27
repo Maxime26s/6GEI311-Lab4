52cbb7e7e73ea58e1af3cc60bb60480deb446de8
Maxime26s <35577093+Maxime26s@users.noreply.github.com>
2021-10-27 10:51:50 -0400

    test: can save tweets (ok)

--- Tests.py
+++ Tests.py
@@ -8,7 +8,7 @@ class TestDatabase(unittest.TestCase):
     def tearDown(self):
         self.db = None
 
-    def test_load_tweets_returns_list_on_error(self):
+    def test_load_tweets_returns_empty_list_on_error(self):
         self.db.tweets = 5
         self.assertTrue(type(self.db.load_tweets()) is list)
         self.assertEqual(self.db.load_tweets(), [])
@@ -17,9 +17,13 @@ class TestDatabase(unittest.TestCase):
         self.assertEqual(self.db.load_tweets(), [])
      
     def test_can_load_tweets_mocked_db(self):
-        self.db.tweets = ["tweet1", "tweet2"]
+        self.db.tweets = [{"tweet1": "test"}, {"tweet2": "test"}]
         self.assertEqual(len(self.db.load_tweets()), 2)
 
+    def test_can_save_tweets(self):
+        self.assertEqual(len(self.db.tweets), 0)
+        self.db.save_tweets([{"tweet1": "test"}, {"tweet2": "test"}])
+        self.assertEqual(len(self.db.tweets), 2)
 
 class TestServer(unittest.TestCase):
     pass

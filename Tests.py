ef7cef61dd88d0d9f290ea2fc0bc954e4e3d5093
Maxime26s <35577093+Maxime26s@users.noreply.github.com>
2021-10-27 10:27:33 -0400

    test_can_load_tweets_default

--- Tests.py
+++ Tests.py
@@ -8,9 +8,13 @@ class TestDatabase(unittest.TestCase):
     def tearDown(self):
         self.db = None
 
-    def test_load_tweets_returns_empty_array_on_error(self):
+    def test_load_tweets_returns_list_on_error(self):
         self.db.tweets = 5
         self.assertTrue(type(self.db.load_tweets()) is list)
+        self.assertEqual(self.db.load_tweets(), [])
+
+    def test_can_load_tweets_default(self):
+        self.assertEqual(self.db.load_tweets(), [])
 
 
 class TestServer(unittest.TestCase):

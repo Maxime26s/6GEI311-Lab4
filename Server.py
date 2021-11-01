6918effffffb61661764327750be58a2e89bb554
Maxime26s <35577093+Maxime26s@users.noreply.github.com>
2021-11-01 15:09:43 -0400

    refactor: Server.py

--- Server.py
+++ Server.py
@@ -27,41 +27,45 @@ class Lab4HTTPRequestHandler(SimpleHTTPRequestHandler):
 
     def do_GET(self):
         if self.path == '/':
-            self.path = 'Search.html'
+            self.route_search()
+        elif self.path.startswith('/queryTwitter'):
+            self.route_display
 
-            return SimpleHTTPRequestHandler.do_GET(self)
+    def route_search(self):
+        self.path = 'Search.html'
+        return SimpleHTTPRequestHandler.do_GET(self)
 
-        if self.path.startswith('/queryTwitter'):
-            data = ''
+    def route_display(self):
+        data = ''
 
-            query_components = parse_qs(urlparse(self.path).query)
-            if 'query' in query_components:
-                data = query_components['query'][0]
+        query_components = parse_qs(urlparse(self.path).query)
+        if 'query' in query_components:
+            data = query_components['query'][0]
 
-            headers = TwitterAPI.create_twitter_headers()
-            url, params = TwitterAPI.create_twitter_url(data)
-            json_response = TwitterAPI.query_twitter_api(url, headers, params)
-            tweets = json_response['data']
+        headers = TwitterAPI.create_twitter_headers()
+        url, params = TwitterAPI.create_twitter_url(data)
+        json_response = TwitterAPI.query_twitter_api(url, headers, params)
+        tweets = json_response['data']
 
-            # Assume that right here, we save the tweets into a SQL databases
-            self.db.save_tweets(tweets)
+        # Assume that right here, we save the tweets into a SQL databases
+        self.db.save_tweets(tweets)
 
-            # Assume that right here, we load the tweets from a SQL database
-            all_tweets = self.db.load_tweets()
+        # Assume that right here, we load the tweets from a SQL database
+        all_tweets = self.db.load_tweets()
 
-            tweets_to_display = ''
-            for tweet in all_tweets:
-                tweets_to_display += '<div> <li>' + tweet['text'] + '</li> </div>'
+        tweets_to_display = ''
+        for tweet in all_tweets:
+            tweets_to_display += '<div> <li>' + tweet['text'] + '</li> </div>'
 
-            text_to_display = ''
-            with open('Display.html', 'r') as file:
-                text_to_display = f"{file.read()}".format(**locals())
+        text_to_display = ''
+        with open('Display.html', 'r') as file:
+            text_to_display = f"{file.read()}".format(**locals())
 
-            self.send_response(HTTPStatus.OK)
-            self.send_header('Content-type', 'text/html')
-            self.end_headers()
+        self.send_response(HTTPStatus.OK)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
 
-            self.wfile.write(text_to_display.encode('utf-8'))
-            self.wfile.close()
+        self.wfile.write(text_to_display.encode('utf-8'))
+        self.wfile.close()
 
-            self.path = 'Display.html'
+        self.path = 'Display.html'
